62bc1517f84634ff92e1d76ed6485a1c4c95f992
koray öngün <koraygg28@gmail.com>
2023-11-03 22:31:08 +0300

    #18

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,13 +15,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.httpClientService.get<Product[]>({controller:"products"}).subscribe(data=>console.table(data))
-    
-    // this.httpClientService.get(
-    //   {
-    //     controller:"products",
-    //     fullEndPoint:"https://jsonplaceholder.typicode.com/posts"
-    //   }).subscribe(data=>console.log(data));
+
   }
 }
 
